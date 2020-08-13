6a003b18081d3b4322feaf84bdc89d3cfc3d2a0b
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2020-08-14 00:10:40 +0600

    Fishing all variables out

---
+++ index.php
@@ -0,0 +1,32 @@
+<!-- Declaring a variable -->
+
+<?php
+
+  $name = "kola";
+  echo $name;
+  echo "<br>";
+  echo "<br>";
+
+?>
+
+<!-- Joining a variable with a string -->
+
+<?php
+
+ $name = "kamranga";
+ echo $name." khayte tok!";
+
+
+?>
+
+ <!-- Array -->
+<?php
+  
+  echo "<br>";
+  echo "<br>";
+  $names = array("Duck", "Luck", "Chuck");
+  echo $names['1'];
+
+
+?>
+
