427e12f13ab7baa460c67d53e018de2d84f9a773
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2020-08-25 00:20:29 +0600

    Hey you, yes you, you're always stuck in my head

---
+++ prepared_statements_and_stuffs.php
@@ -0,0 +1,42 @@
+<?php
+
+include 'includes/dbh.inc.php';
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+
+  <?php
+    $data = "yw";
+    //Created a template
+   $sql = "SELECT * FROM users WHERE user_uid=?;";
+   //Created a prepared statement
+   $stmt = mysqli_stmt_init($conn);
+   //Prepare the prepared statement
+   if (!mysqli_stmt_prepare($stmt, $sql)) {
+     echo "SQL statement failed";
+   } else {
+     // Bind parameters to the placeholders
+     mysqli_stmt_bind_param($stmt, "s", $data);
+     //Run parameters inside database
+     mysqli_stmt_execute($stmt);
+     $result = mysqli_stmt_get_result($stmt);
+
+     while ($row = mysqli_fetch_assoc($result)) {
+       echo $row['user_uid'] . "<br>";
+     }
+
+ }
+
+?>
+
+  </body>
+</html>
---
+++ protecting_data_against_sql_injection_using_mysqli.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+<form action="includes/signup.inc.prepared.php" method="POST">
+  <input type="text" name="first" placeholder="First Name"> <br>
+  <input type="text" name="last" placeholder="Last Name"> <br>
+  <input type="text" name="email" placeholder="E-mail"> <br>
+  <input type="text" name="uid" placeholder="Username"> <br>
+  <input type="text" name="pwd" placeholder="Password"> <br>
+  <button type="submit" name="submit">Sign Up</button>
+</form>
+
+
+
+  </body>
+</html>
